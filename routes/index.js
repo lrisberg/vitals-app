87b811da5a54267cf50f9355072b56cf7e3b9755
JohnG585 <jgallagher585@gmail.com>
2017-04-08 15:39:58 -0600

    add signup.js and start of login stuff

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
