392bb3713cd4647deefc6de93c575124bcd9bd50
JohnG585 <jgallagher585@gmail.com>
2017-04-07 21:05:14 -0600

    created seeds

---
+++ seeds/004_notes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('notes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('notes').insert([
+        {
+          id:1,
+          user_id: 1,
+          doctor_id: 1,
+          body: "He kept saying 'That's what shee said' over and over."
+        }
+      ]);
+    })
+    .then(() => {
+      return knex.raw("SELECT setval('notes_id_seq', (SELECT MAX(id) FROM notes));");
+    });
+};
