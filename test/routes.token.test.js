0a7d7b329f2294564503f160b1010fc4ad9307f1
Lauren Risberg <lrisberg@users.noreply.github.com>
2017-04-09 10:33:37 -0600

    add first routes test

---
+++ test/routes.token.test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.env.NODE_ENV = 'development';
+
+const assert = require('chai').assert;
+const { suite, test } = require('mocha');
+//const bcrypt = require('bcrypt');
+const request = require('supertest');
+const knex = require('../knex');
+const app = require('../app');
+
+suite('routes/token', () => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  beforeEach((done) => {
+    knex.seed.run()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('POST /token', () => {
+    const password = 'oatyoatyoats';
+
+    request(app)
+      .post('/users')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({
+        email: 'nonexist@user.com',
+        password
+      })
+      .expect(400, 'Bad email or password')
+      .end((httpErr, _res) => {
+        done();
+      });
+
+  })
+
+
+
+})
