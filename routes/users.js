6be1fc32cf311c64d4686141fa4dfc00e5d0674d
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-04-08 21:21:58 -0600

    clicking signup button adds user's info to database

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,32 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const knex = require('../knex');
+const humps = require('humps');
 
 /* GET users listing. */
-router.post('/users', (req, res, next) => {
-  console.log(req.body);
-})
+router.post('/', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      return knex('users')
+        .insert({
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          age: req.body.age,
+          gender: req.body.gender,
+          height: req.body.height,
+          weight: req.body.weight,
+          hashed_password: hashed_password
+        }, '*');
+    })
+    .then((users) => {
+      const user = users[0];
+      delete user.hashed_password;
+      res.send(humps.camelizeKeys(user));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
