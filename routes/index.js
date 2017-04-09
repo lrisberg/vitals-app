43095705490dfceab2438f91cbd1547e4ad8d955
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-04-08 18:44:30 -0600

    add signup to its own route file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcrypt');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/signup', function(req, res, next) {
-  res.render('signup');
-})
 
 module.exports = router;
