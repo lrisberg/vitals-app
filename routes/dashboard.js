f2ebd68b45e0caa6f046def187f43e55ea43fc23
Lauren Risberg <lauren.risberg@gmail.com>
2017-04-10 14:08:40 -0600

    user's firstname is rendered to the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,16 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const jwt = require('jsonwebtoken');
 
-/* GET home page. */
-router.get('/dashboard', function(req, res, next) {
-  if (!req.cookies.token) {
-    res.header('Content-Type', 'text/plain');
-    res.status(401).send('Unauthorized');
-  } else {
-    res.render('dashboard', {
-      title: 'Welcome!'
+const SECRET = process.env.JWT_KEY || 'itsasecret';
+
+function checkAuth(req, res, next) {
+  if (req.cookies.token) {
+    jwt.verify(req.cookies.token, SECRET, function(err, decoded) {
+      if (err) {
+        res.setHeader("Content-Type", "text/plain");
+        res.status(401);
+        res.send('Unauthorized');
+      } else {
+        req.user = decoded;
+        next();
+      }
     });
+  } else {
+    res.setHeader("Content-Type", "text/plain");
+    res.status(401);
+    res.send('Unauthorized');
   }
+}
+
+
+router.get('/dashboard', checkAuth, (req, res, next) => {
+  knex('users')
+    .where('id', req.user.userId)
+    .then((usersFromKnex) => {
+      let user = usersFromKnex[0]
+      res.render('dashboard', { name: user.first_name });
+    })
 });
 
 module.exports = router;
