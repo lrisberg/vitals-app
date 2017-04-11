c3648addeb701af8d58ce1f485b6d568e7d25cab
JohnG585 <jgallagher585@gmail.com>
2017-04-11 16:10:12 -0600

    -

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,11 @@
 $(document).ready(() => {
+  (function() {
   $('.carousel').carousel();
    $('.col s1 offset-4 dropdown-button btn').dropdown('open');
 
-  document.addEventListener('DOMContentLoaded', function(){
-      Typed.new('.element', {
-        strings: ["First sentence.", "Second sentence."],
-        typeSpeed: 0
-      });
-  });
-</script>
-...
+   $(".navbar-brand").hover(function () {
+     $(this).toggleClass("animated shake");
+   });
 
-<div class="element"></div>
+  })();
 });
