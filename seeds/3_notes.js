32d7e8925e3d0a06bca6774419850066fa9d4ee2
Lauren Risberg <lauren.risberg@gmail.com>
2017-04-11 13:41:12 -0600

    simplified schema with 'categories' changed to 'records'

--- seeds/3_notes.js
+++ seeds/3_notes.js
@@ -1,4 +1,3 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('notes').del()
@@ -8,54 +7,54 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           user_id: 1,
-          category_id: 1,
+          record_id: 1,
           body: "He kept saying 'That's what she said' over and over."
         },
         {
           id: 2,
           user_id: 1,
-          category_id: 2,
+          record_id: 2,
           body: "The dentist told me to floss more."
         },
         {
           id: 3,
           user_id: 2,
-          category_id: 3,
+          record_id: 3,
           body: "I had a baby!"
         },
         {
           id: 4,
           user_id: 3,
-          category_id: 4,
+          record_id: 4,
           body: "I went in to the clinic for a flu shot."
         },
         {
           id: 5,
           user_id: 3,
-          category_id: 4,
+          record_id: 4,
           body: "Then I had some blood work done."
         },
         {
           id: 6,
           user_id: 3,
-          category_id: 8,
+          record_id: 8,
           body: "The doctor gave me a lollypop!"
         },
         {
           id: 7,
           user_id: 2,
-          category_id: 12,
+          record_id: 12,
           body: "I was prescribed some drugs."
         },
         {
           id: 8,
           user_id: 1,
-          category_id: 11,
+          record_id: 11,
           body: "Blood work came back good."
         }
       ]);
     })
     .then(() => {
-      return knex.raw("SELECT setval('categories_id_seq', (SELECT MAX(id) FROM categories));");
+      return knex.raw("SELECT setval('records_id_seq', (SELECT MAX(id) FROM records));");
     });
 };
