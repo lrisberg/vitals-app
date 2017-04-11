ba91689a0694a88d3302dd7ba55ae68c6e854291
JohnG585 <jgallagher585@gmail.com>
2017-04-11 11:54:31 -0600

    -

--- migrations/20170407173947_specialties.js
+++ migrations/20170407173947_specialties.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('specialties', (table) => {
     table.increments();
     table.string('name').notNullable();
-    table.text('logo').notNullable()
+    table.text('logo').notNullable().defaultTo('https://www.steward.org/sites/default/files/stock%20doctors.png')
   })
 };
 
