dc07666ea2415abf12022c9d5e4bffeec78b95a8
JohnG585 <jgallagher585@gmail.com>
2017-04-11 15:27:40 -0600

    fixed signup

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(() => {
-  console.log('hello!');
   $('.carousel').carousel();
    $('.col s1 offset-4 dropdown-button btn').dropdown('open');
+
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.element', {
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 0
+      });
+  });
+</script>
+...
+
+<div class="element"></div>
 });
